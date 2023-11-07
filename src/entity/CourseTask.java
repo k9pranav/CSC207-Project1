daf17d6094a99e9674708aafc4ecf4166efb6787
Pranav Kansal <k9pranav>
2023-11-07 14:35:57 -0500

    Added and edited task entitities. Namely: Task (Abstract), Student Task and CourseTask

--- src/entity/CourseTask.java
+++ src/entity/CourseTask.java
@@ -1,5 +1,41 @@
 package entity;
 
-public class CourseTask {
+import java.text.SimpleDateFormat;
+
+public class CourseTask extends Task implements CreateCourseTask {
+
+    private final Course taskCourse;
+
+    private Float taskGrade;
+
+    private Float taskWeight;
+
+    private Boolean gradeReleased;
+
+
+    public CourseTask(String taskName, String type, SimpleDateFormat deadLine,
+                      Course taskCourse, Float taskWeight) {
+        super(taskName, type, deadLine);
+        this.taskCourse = taskCourse;
+        this.taskWeight = taskWeight;
+        this.gradeReleased = false;
+
+    }
+
+    public Course getCourse(){return taskCourse;}
+
+    public Float getGrade(){return taskGrade;}
+
+    public void setGrade(Float newGrade){taskGrade = newGrade;}
+
+    @Override
+    public void setWeight(Float newWeight){taskWeight = newWeight;}
+
+    @Override
+    public void setDeadline(SimpleDateFormat newDeadline) {
+        deadLine = newDeadline;
+    }
+
+
     // need getGrade method
 }
--- src/entity/CreateCourseTask.java
+++ src/entity/CreateCourseTask.java
@@ -1,4 +1,9 @@
 package entity;
 
+import java.text.SimpleDateFormat;
+
 public interface CreateCourseTask {
+
+    void setDeadline(SimpleDateFormat newDeadline);
+    void setWeight(Float weight);
 }
