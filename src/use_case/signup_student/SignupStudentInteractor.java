c8319a7f346f96b0cb571ee48addcdd9aba6388f
nadjamarjanovic <129619501+nadjamarjanovic@users.noreply.github.com>
2023-11-10 16:21:26 -0500

    use_case files for student sign up use case

--- src/use_case/signup_student/SignupStudentInteractor.java
+++ src/use_case/signup_student/SignupStudentInteractor.java
@@ -1,4 +1,39 @@
 package use_case.signup_student;
 
-public class SignupStudentInteractor {
+import entity.Student;
+import entity.StudentFactory;
+import use_case.signup_admin.SignupAdminOutputData;
+
+public class SignupStudentInteractor implements SignupStudentInputBoundary{
+    final SignupStudentDataAccessInterface userDataAccessObject;
+    final SignupStudentOutputBoundary userPresenter;
+    Student student;
+
+    public SignupStudentInteractor(SignupStudentDataAccessInterface userDataAccessObject,
+                                 SignupStudentOutputBoundary userPresenter,
+                                 StudentFactory studentFactory){
+        this.userDataAccessObject = userDataAccessObject;
+        this.userPresenter = userPresenter;
+        this.student = student;
+    }
+
+    @Override
+    public void execute(SignupStudentInputData signupStudentInputData) {
+        if (userDataAccessObject.existsByEmail(signupStudentInputData.getEmail())){
+            userPresenter.prepareFailView("User Already Exists");
+        } else if (!signupStudentInputData.getPassword().equals(signupStudentInputData.getRepeatPassword())) {
+            userPresenter.prepareFailView("Passwords Don't Match");
+        } else {
+            Admin admin = new Student(signupStudentInputData.getFirstName(),
+                    signupStudentInputData.getLastName(), signupStudentInputData.getPassword(),
+                    signupStudentInputData.getRepeatPassword(), signupStudentInputData.getEmail());
+
+            userDataAccessObject.save(student);
+            userDataAccessObject.createNewCalendar(student);
+
+            SignupAdminOutputData signupAdminOutputData = new SignupAdminOutputData(admin.getFirstName(),
+                    admin.getLastName(), admin.getEmail(), false);
+        }
+
+    }
 }
