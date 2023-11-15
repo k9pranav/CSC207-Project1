c16433787b9d87cb6770d3943f0cd613ecc7c327
nadjamarjanovic <129619501+nadjamarjanovic@users.noreply.github.com>
2023-11-15 14:22:56 -0500

    basic implementation of interface adapters and view for admin login use case

---
+++ src/interface_adapter/login_admin/LoginAdminController.java
@@ -0,0 +1,5 @@
+package interface_adapter.login_admin;
+
+public class LoginAdminController {
+    public void execute(String s){}
+}
--- src/interface_adapter/login_admin/LoginAdminState.java
+++ src/interface_adapter/login_admin/LoginAdminState.java
@@ -1,4 +1,26 @@
 package interface_adapter.login_admin;
 
 public class LoginAdminState {
+    private String username = "";
+    private String usernameError = null;
+    private String password = "";
+    private String passwordError = null;
+
+    public LoginAdminState(LoginAdminState copy){
+        username = copy.username;
+        usernameError = copy.usernameError;
+        password = copy.password;
+        passwordError = copy.passwordError;
+    }
+
+    public LoginAdminState(){}
+
+    public String getUsername(){return username;}
+    public String getUsernameError(){ return usernameError;}
+    public String getPassword(){return password;}
+    public String getPasswordError(){return passwordError;}
+    public void setUsername(String username){this.username = username;}
+    public void setUsernameError(String usernameError){this.usernameError = usernameError;}
+    public void setPassword(String password){this.password = password;}
+    public void setPasswordError(String passwordError){this.passwordError = passwordError;}
 }
