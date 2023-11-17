191249fc76aaf93268d0a7a6ee6f4a0f18f63425
nadjamarjanovic <129619501+nadjamarjanovic@users.noreply.github.com>
2023-11-16 20:37:10 -0500

    beginning implementation of logged in view/viewmodel/state for admin users

---
+++ src/interface_adapter/admin_logged_in/AdminLoggedInViewModel.java
@@ -0,0 +1,15 @@
+package interface_adapter.admin_logged_in;
+
+public class AdminLoggedInViewModel {
+    public AdminLoggedInState getState() {
+    }
+
+    public void setState(AdminLoggedInState loggedInState) {
+    }
+
+    public void firePropertyChanged() {
+    }
+
+    public String getViewName() {
+    }
+}
