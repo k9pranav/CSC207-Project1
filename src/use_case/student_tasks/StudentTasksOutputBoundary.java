0ce70314a0a0a1f1c66704bbab5011029398c0c4
nadjamarjanovic <129619501+nadjamarjanovic@users.noreply.github.com>
2023-11-18 17:55:40 -0500

    implemented output data boundary, output data object, and finished use case interactor

--- src/use_case/student_tasks/StudentTasksOutputBoundary.java
+++ src/use_case/student_tasks/StudentTasksOutputBoundary.java
@@ -1,4 +1,10 @@
 package use_case.student_tasks;
 
+import entity.StudentTask;
+
 public interface StudentTasksOutputBoundary {
+    void prepareTaskPopup(StudentCourseTasksOutputData outputData);
+    void prepareTaskPopup(StudentTasksOutputData outputData);
+    void prepareExit();
+    void prepareEditTaskView(StudentTask newTask);
 }
