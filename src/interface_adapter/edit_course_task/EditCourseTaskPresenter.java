702246cb818ef6572bd74459f5cad33e29cbde33
Pranav Kansal <k9pranav>
2023-11-26 20:19:50 -0500

    Basic implementatin of Edit Course Tasks. Need to do DAO, and sort the pre-synced values when admin edits tasks, instead of creating a new one

--- src/interface_adapter/edit_course_task/EditCourseTaskPresenter.java
+++ src/interface_adapter/edit_course_task/EditCourseTaskPresenter.java
@@ -1,4 +1,48 @@
 package interface_adapter.edit_course_task;
 
-public class EditCourseTaskPresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.admin_tasks.AdminTasksState;
+import interface_adapter.admin_tasks.AdminTasksViewModel;
+import use_case.edit_course_task.EditCourseTaskOutputBoundary;
+import use_case.edit_course_task.EditCourseTaskOutputData;
+import view.ViewManager;
+
+public class EditCourseTaskPresenter  implements EditCourseTaskOutputBoundary {
+
+    private final EditCourseTaskViewModel editCourseTaskViewModel;
+
+    private final AdminTasksViewModel adminTasksViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public EditCourseTaskPresenter(EditCourseTaskViewModel editCourseTaskViewModel,
+                                   AdminTasksViewModel adminTasksViewModel,
+                                   ViewManagerModel viewManagerModel) {
+        this.editCourseTaskViewModel = editCourseTaskViewModel;
+        this.adminTasksViewModel = adminTasksViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+
+    @Override
+    public void prepareSuccessView(EditCourseTaskOutputData courseTask) {
+        //On success, go to admin tasks in view
+
+        AdminTasksState adminTasksState = adminTasksViewModel.getState();
+        this.adminTasksViewModel.setState(adminTasksState);
+        this.adminTasksViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(adminTasksViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+
+
+    }
+
+    @Override
+    public void prepareFailView(String errorstring) {
+
+        EditCourseTaskState editCourseTaskState = editCourseTaskViewModel.getState();
+        editCourseTaskState.setError(errorstring);
+        editCourseTaskViewModel.firePropertyChanged();
+
+    }
 }
--- src/interface_adapter/edit_course_task/EditCourseTaskViewModel.java
+++ src/interface_adapter/edit_course_task/EditCourseTaskViewModel.java
@@ -1,4 +1,39 @@
 package interface_adapter.edit_course_task;
 
-public class EditCourseTaskViewModel {
+import interface_adapter.ViewModel;
+import view.EditCourseTaskView;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+import java.beans.PropertyChangeListener;
+
+public class EditCourseTaskViewModel extends ViewModel {
+
+    public static final String SAVE_BUTTON_LABEL = "Save";
+    public static final String EXIT_BUTTON_LABEL = "Exit";
+
+    private static EditCourseTaskState state = new EditCourseTaskState();
+
+    public EditCourseTaskViewModel(){super("Edit Task Course");}
+    public void setState(EditCourseTaskState state) {
+        this.state = state;
+    }
+
+    public static EditCourseTaskState getState() {
+        return state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
 }
