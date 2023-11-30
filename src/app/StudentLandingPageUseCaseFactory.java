3ffa11a6cce6800eef77136a1d2a0ec8acf33c85
nadjamarjanovic <129619501+nadjamarjanovic@users.noreply.github.com>
2023-11-29 20:00:10 -0500

    student landing page use case factory implemented

--- src/app/StudentLandingPageUseCaseFactory.java
+++ src/app/StudentLandingPageUseCaseFactory.java
@@ -1,4 +1,28 @@
 package app;
 
+import interface_adapter.ViewManagerModel;
+import interface_adapter.login_student.LoginStudentViewModel;
+import interface_adapter.signup_student.SignupStudentViewModel;
+import interface_adapter.student_landing_page.StudentLandingPageController;
+import interface_adapter.student_landing_page.StudentLandingPagePresenter;
+import interface_adapter.student_landing_page.StudentLandingPageViewModel;
+import use_case.student_landing_page.StudentLandingPageInputBoundary;
+import use_case.student_landing_page.StudentLandingPageOutputBoundary;
+import use_case.student_landing_page.StudentLandingPageUseCaseInteractor;
+import view.StudentLandingPageView;
+
 public class StudentLandingPageUseCaseFactory {
+    public static StudentLandingPageView create(ViewManagerModel viewManagerModel, StudentLandingPageViewModel studentLandingPageViewModel, SignupStudentViewModel studentSignupViewModel, LoginStudentViewModel studentLoginViewModel) {
+        StudentLandingPageController studentLandingPageController = createStudentLandingPageUseCase(viewManagerModel, studentLandingPageViewModel, studentLoginViewModel, studentSignupViewModel);
+        return new StudentLandingPageView(studentLandingPageController, studentLandingPageViewModel);
+    }
+
+    private static StudentLandingPageController createStudentLandingPageUseCase(ViewManagerModel viewManagerModel, StudentLandingPageViewModel studentLandingPageViewModel, LoginStudentViewModel loginStudentViewModel, SignupStudentViewModel signupStudentViewModel) {
+        StudentLandingPageOutputBoundary studentLandingPageOutputBoundary = new StudentLandingPagePresenter(viewManagerModel, studentLandingPageViewModel, signupStudentViewModel, loginStudentViewModel);
+
+        StudentLandingPageInputBoundary landingPageInteractor = new StudentLandingPageUseCaseInteractor(studentLandingPageOutputBoundary);
+        return new StudentLandingPageController(landingPageInteractor);
+
+    }
 }
+
