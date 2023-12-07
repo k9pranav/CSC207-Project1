913fda6c65fd00e89a31a7a7f18ff561ba12cbef
lasandaDewamuni <132021752+lasandaDewamuni@users.noreply.github.com>
2023-12-07 11:50:27 -0500

    Updated day to format the task hashmap as HashMap<String, Task> Implmented StudentCalendarView

--- src/view/StudentCalendarView.java
+++ src/view/StudentCalendarView.java
@@ -5,17 +5,21 @@ import interface_adapter.go_back_student.GoBackStudentController;
 import interface_adapter.go_back_student.GoBackStudentState;
 import interface_adapter.go_back_student.GoBackStudentViewModel;
 import interface_adapter.student_tasks.StudentTasksController;
+import interface_adapter.student_tasks.StudentTasksState;
 import interface_adapter.student_tasks.StudentTasksViewModel;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
+import java.util.*;
 
 public class StudentCalendarView extends JPanel implements ActionListener, PropertyChangeListener {
 
@@ -53,19 +57,35 @@ public class StudentCalendarView extends JPanel implements ActionListener, Prope
         Student student = tasksViewModel.getState().getLoggedInUser();
 
         this.allTasks = student.getTasks();
-        updateEventYearMap(allTasks);
+        //updateEventYearMap(allTasks);
 
 
+        JPanel contentPane = new JPanel(new BorderLayout());
+        this.add(contentPane);
+
+        JPanel headerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton prevButton = new JButton("<< Prev");
+        prevButton.addActionListener(e -> navigateMonth(-1));
+        JButton nextButton = new JButton("Next >>");
+        nextButton.addActionListener(e -> navigateMonth(1));
+        monthLabel = new JLabel("", SwingConstants.CENTER);
+        headerPanel.add(prevButton);
+        headerPanel.add(monthLabel);
+        headerPanel.add(nextButton);
+        contentPane.add(headerPanel, BorderLayout.NORTH);
+
+        daysPanel = new JPanel(new GridLayout(0, 7, 5, 5));
+        contentPane.add(daysPanel, BorderLayout.CENTER);
 
 
-
-
-        this.goBackBtn = new JButton("Go Back");
+        this.goBackBtn = new JButton();
         this.footer = new JPanel();
+        footer.setPreferredSize(new Dimension(4000, 100));
         this.email = goBackStudentViewModel.getState().getEmail();
         this.accountType = goBackStudentViewModel.getState().getEmail();
 
         footer.add(goBackBtn);
+        this.add(footer, BorderLayout.SOUTH);
 
         goBackBtn.addActionListener(
                 new ActionListener(){
@@ -79,7 +99,113 @@ public class StudentCalendarView extends JPanel implements ActionListener, Prope
                     }
                 }
         );
+
+        currentDate = LocalDate.now();
+        displayMonth(currentDate);
     }
+
+    private void displayMonth(LocalDate date) {
+        updateEventYearMap(allTasks);
+
+        monthLabel.setText(date.getMonth().toString() + " " + date.getYear());
+
+        daysPanel.removeAll();
+        for (DayOfWeek day : DayOfWeek.values()) {
+            JLabel label = new JLabel(day.toString().substring(0, 3), SwingConstants.CENTER);
+            daysPanel.add(label);
+        }
+
+        LocalDate firstDayOfMonth = date.withDayOfMonth(1);
+        int emptySlots = firstDayOfMonth.getDayOfWeek().getValue() % 7;
+        for (int i = 0; i < emptySlots; i++) {
+            daysPanel.add(new JLabel());
+        }
+
+        LocalDate tempDate = firstDayOfMonth;
+        while (tempDate.getMonth().equals(date.getMonth())) {
+            JPanel dayPanel = new JPanel();
+            dayPanel.setBackground(new Color(200,0,0,255));
+            dayPanel.setLayout(new BoxLayout(dayPanel, BoxLayout.Y_AXIS));
+
+            JLabel dayLabel = new JLabel(String.valueOf(tempDate.getDayOfMonth()), SwingConstants.CENTER);
+            dayLabel.setBorder(new EmptyBorder(5, 5, 5, 5));
+            dayPanel.add(dayLabel);
+
+            DefaultListModel<String> listModel = new DefaultListModel<>();
+            JList<String> list = new JList<>(listModel);
+            addTaskToList(listModel, date);
+
+
+            list.addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent evt) {
+                    if (evt.getClickCount() == 2) {
+                        tasksController.execute(list.getSelectedValue(), tasksViewModel.getState().getLoggedInUser());
+
+                    }
+                }
+            });
+
+            JScrollPane scrollPane = new JScrollPane();
+            dayPanel.add(scrollPane);
+            scrollPane.add(list);
+
+            daysPanel.add(dayPanel);
+            tempDate = tempDate.plusDays(1);
+        }
+
+        revalidate();
+        repaint();
+    }
+
+    private void addTaskToList(DefaultListModel listModel, LocalDate date) {
+        if(yearHashMap.containsKey(date.getYear())){
+            Year year = yearHashMap.get(date.getYear());
+            if(year.hasMonth(date.getMonthValue() - 1)){
+                Month month = year.getMonth(date.getMonthValue() - 1);
+                for(Week week : month){
+                    for (Day day : week){
+                        if (day.getDayOfTheMonth() == date.getDayOfMonth()){
+                            ArrayList<String> sortedList = getSortedTaskTimes(day);
+                            for (String time: sortedList) {
+                                listModel.addElement(day.getTask(time).getTaskName() + "Time: " + time);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static ArrayList<String> getSortedTaskTimes(Day day) {
+        Set<String> taskDeadlines = day.getMapOfTasks().keySet();
+        ArrayList<String> sortedList = new ArrayList<>(taskDeadlines);
+        sortedList.sort(new Comparator<String>() {
+            @Override
+            public int compare(String time1, String time2) {
+                String[] time1Parts = time1.split(":");
+                String[] time2Parts = time2.split(":");
+
+                for (int i = 0; i < 3; i++) {
+                    int timePart1 = Integer.parseInt(time1Parts[i]);
+                    int timePart2 = Integer.parseInt(time2Parts[i]);
+
+                    if (timePart1 != timePart2) {
+                        return Integer.compare(timePart1, timePart2);
+                    }
+                }
+
+                return 0;
+            }
+        });
+        return sortedList;
+    }
+
+    private void navigateMonth(int offset) {
+        currentDate = currentDate.plusMonths(offset);
+        displayMonth(currentDate);
+    }
+
 
 
     private void updateEventYearMap( ArrayList<Task> allTasks){
@@ -103,12 +229,12 @@ public class StudentCalendarView extends JPanel implements ActionListener, Prope
 
                         if (deadlineWeekObj.hasDay(deadlineDayOfWeek)){
                             Day deadlineDayObj = deadlineWeekObj.getDay(deadlineDayOfWeek);
-                            deadlineDayObj.addTasks(allTasks.get(i).getTaskName(), allTasks.get(i));
+                            deadlineDayObj.addTasks(allTasks.get(i).getDeadLine(), allTasks.get(i));
 
                             deadlineWeekObj.addDays(deadlineDayObj);
                         } else {
                             Day deadlineDayObj = DayFactory.create(deadlineMonthObj, deadlineDay, deadlineDayOfWeek );
-                            deadlineDayObj.addTasks(allTasks.get(i).getTaskName(), allTasks.get(i));
+                            deadlineDayObj.addTasks(allTasks.get(i).getDeadLine(), allTasks.get(i));
 
                             deadlineWeekObj.addDays(deadlineDayObj);
                         }
@@ -117,7 +243,7 @@ public class StudentCalendarView extends JPanel implements ActionListener, Prope
                     } else {
                         Week deadlineWeekObj = WeekFactory.create(deadlineMonthObj, deadlineWeek);
                         Day deadlineDayObj = DayFactory.create(deadlineMonthObj, deadlineDay, deadlineDayOfWeek );
-                        deadlineDayObj.addTasks(allTasks.get(i).getTaskName(), allTasks.get(i));
+                        deadlineDayObj.addTasks(allTasks.get(i).getDeadLine(), allTasks.get(i));
                         deadlineWeekObj.addDays(deadlineDayObj);
 
                         deadlineMonthObj.addWeeks(deadlineWeekObj);
@@ -127,7 +253,7 @@ public class StudentCalendarView extends JPanel implements ActionListener, Prope
                     Month deadlineMonthObj = MonthFactory.create(deadlineMonth, deadlineYearObj.getYear());
                     Week deadlineWeekObj = WeekFactory.create(deadlineMonthObj, deadlineWeek);
                     Day deadlineDayObj = DayFactory.create(deadlineMonthObj, deadlineDay, deadlineDayOfWeek );
-                    deadlineDayObj.addTasks(allTasks.get(i).getTaskName(), allTasks.get(i));
+                    deadlineDayObj.addTasks(allTasks.get(i).getDeadLine(), allTasks.get(i));
                     deadlineWeekObj.addDays(deadlineDayObj);
                     deadlineMonthObj.addWeeks(deadlineWeekObj);
 
@@ -140,7 +266,7 @@ public class StudentCalendarView extends JPanel implements ActionListener, Prope
                 Month deadlineMonthObj = MonthFactory.create(deadlineMonth, deadlineYearObj.getYear());
                 Week deadlineWeekObj = WeekFactory.create(deadlineMonthObj, deadlineWeek);
                 Day deadlineDayObj = DayFactory.create(deadlineMonthObj, deadlineDay, deadlineDayOfWeek );
-                deadlineDayObj.addTasks(allTasks.get(i).getTaskName(), allTasks.get(i));
+                deadlineDayObj.addTasks(allTasks.get(i).getDeadLine(), allTasks.get(i));
                 deadlineWeekObj.addDays(deadlineDayObj);
                 deadlineMonthObj.addWeeks(deadlineWeekObj);
                 deadlineYearObj.addMonth(deadlineMonthObj);
@@ -152,7 +278,7 @@ public class StudentCalendarView extends JPanel implements ActionListener, Prope
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        System.out.println("Click" + e.getActionCommand());
     }
 
     /**
@@ -163,6 +289,10 @@ public class StudentCalendarView extends JPanel implements ActionListener, Prope
      */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-
+        String propName = evt.getPropertyName();
+        if (propName.equals("popup")) {
+            StudentTasksState currState = (StudentTasksState) evt.getNewValue();
+            JOptionPane.showMessageDialog(this, currState.getCurrentTaskInfo());
+        }
     }
 }
